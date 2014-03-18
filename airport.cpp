efe08f7715a47d7832def7d795adf91702eaf447
odingrey <odingrey@gmail.com>
2014-03-17 23:53:01 -0700

    Finished debugging, moved Nodes back to their original place

--- airport.cpp
+++ airport.cpp
@@ -7,27 +7,33 @@
 using namespace std;
 
 
+
+void debug(HubNode*);
+void printMain();
+void printItinerary(string, string, string, string, double, int, double, Date_Time*, Date_Time*);
+
 HubNode* hub = NULL;
+
+
+
+int main(){
+	//do stuff here
+	printMain();
+	debug(hub);
+	return 0;
+}
+
+
 
 void printMain(){	
 		printf("Welcome to the Airport Reservation Extravaganza!!\n\n");
 		printf("Please select from the following options:\n");
 }
 
-
-int main(){
-	//do stuff here
-	printMain();
-	return 0;
-}
-
-
-
-
-void printItinerary(string FlightNum, string Company, string SourceLocation, string DestinationLocation, double Price, int NumOfBags, double TotalPrice){
+void printItinerary(string FlightNum, string Company, string SourceLocation, string DestinationLocation, double Price, int NumOfBags, double TotalPrice, Date_Time* Arrive_DateTime, Date_Time* Depart_DateTime){
 	TotalPrice = NumOfBags * Price;
 
-	//printf("%s\t%s\t%s\t%s\n\t\t%s\t%s\n%f=%f",FlightNum, Company, SourceLocation, Depart_DateTime, DestinationLocation, Arrive_DateTime, Price, TotalPrice); 
+	printf("%s\t%s\t%s\t%s\n\t\t%s\t%s\n%f=%f",FlightNum.c_str(), Company.c_str(), SourceLocation.c_str(), Depart_DateTime->toString().c_str(), DestinationLocation.c_str(), Arrive_DateTime->toString().c_str(), Price, TotalPrice); 
 }
 
 
@@ -38,7 +44,7 @@ void debug(HubNode* hub){
 		while (flight != NULL){		//loops for flight per hub
 			HubNode* source = flight->getSource();
 			HubNode* dest = flight->getDestination();
-			//printf("%d : %d : %s : %s : %s\n", flight->getFlightNumber(), flight->getPrice(), flight->getFlightCompany(), source->getName(), dest->getName());
+			printf("%s : %f : %s : %s : %s\n", flight->getFlightNumber().c_str(), flight->getPrice(), flight->getFlightCompany().c_str(), source->getName().c_str(), dest->getName().c_str());
 			flight = flight->Next();
 		}
 		current = current->Next();
