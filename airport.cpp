6b27b0d1d97793325b23df725b8c335b9c291626
vegetable <vegetable@vegetable.resnetn.asu.edu>
2014-03-17 22:14:06 -0700

    Merging changes.

--- airport.cpp
+++ airport.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <fstream>
 #include "HubNode.h"
 #include "FlightNode.h"
 using namespace std;
@@ -19,12 +20,48 @@ class Date_Time{
 
 
 */
-void main(){
-	//do stuff here
+void main() {
+	HubNode head = new HubNode()
+
+	load_hubs()
+	load_flights()
 }
 
+void load_hubs() {
+	string line;
+	int pos;
+	HubNode current;
+	ifstream hub_file("Hub.csv")
 
+	if (hub_file) {
+		// Skip the header labels
+		getline(hub_file, line);
+		
+		// Initialize head hub node
+		getline(hub_file, line);
+		current = new HubNode();
+		pos = line.find(",");
+		current.name = line.substr(0, pos)
+		current.location = hub_stream.getline(pos+1);
 
+		while (getline(hub_file, line)) {
+			HubNode node = new HubNode();
+			pos = line.find(",");
+			node.name = line.substr(0, pos)
+			node.location = hub_stream.getline(pos+1);
+			node.next = NULL;
+			current.next = node;
+			current = node
+		}
+	} else {
+		cerr << "File Hub.csv could not be opened." << endl
+		exit(1)
+	}
+}
+
+void load_flights() {
+	
+}
 
 void debug(hub){
 	while (hub != NULL){		//loops for hubs
