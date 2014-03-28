482962fc25ee129e529d0a7a669e67be64d08e78
Michael Sprague <odingrey@gmail.com>
2014-03-28 14:10:54 -0700

    Merging Mike and Tom

---
+++ User.h
@@ -0,0 +1,43 @@
+#include "ReservationList.h"
+using namespace std;
+
+class User{
+	ReservationList* reservationHead = NULL;
+	int numOfBags = 0;
+	public:
+	User(){	
+	}
+	int getBagNum(){return this->numOfBags;}
+	Reservation* getReservationHead() {
+		return this->reservationHead;
+	}
+
+	ReservationList* cheapestRoute(){
+		ReservationList* temp = reservationHead;
+		ReservationList* cheapest = temp;
+		if (cheapest == NULL)
+			return NULL;
+		while (temp->Next() != NULL){
+			temp = temp->Next();
+			if(temp->getPrice() < cheapest->getPrice())
+				cheapest = temp;
+			
+		}
+		
+		return cheapest;
+	}
+	
+	ReservationList* shortestRoute(){
+		ReservationList* temp = reservationHead;
+		ReservationList* shortest = temp;
+		if (shortest == NULL)
+			return NULL;
+		while (temp->Next() != NULL){
+			temp = temp->Next();
+			if(temp->getTime() < shortest->getTime())
+				shortest = temp;
+		}
+		
+		return shortest;
+	}
+};
